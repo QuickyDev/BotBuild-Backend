ed167f8bff8b9729bac278d9ac524c784dd96137
Swarit Choudhari <68472469+SwiftyProgrammer690@users.noreply.github.com>
2021-04-13 16:14:49 -0400

    All the Magic happens Here!

---
+++ Robot.java
@@ -0,0 +1,132 @@
+public class Robot {
+
+
+    //Class Variables Used to Create New Robot
+    static String name;
+    static String color;
+    static String functionality;
+
+    //introduceSelf() to introduce bot to user
+    public static void introduceSelfToPythonBot() {
+        System.out.println("This robots name is " + name + ". This bots color is " + color + ". This bot helps you with " + functionality + ".");
+    }
+
+    public static void introduceSelfToJavaBot() {
+        System.out.println("This robots name is " + name + ". This bots color is " + color + ". This bot helps you with " + functionality + ".");
+    }
+    public static void introduceSelfToRubyBot() {
+        System.out.println("This robots name is " + name + ". This bots color is " + color + ". This bot helps you with " + functionality + ".");
+    }
+
+    public static void introduceSelfToHTMLBot() {
+        System.out.println("This robots name is " + name + ". This bots color is " + color + ". This bot helps you with " + functionality + ".");
+    }
+
+    public static void introduceSelfToCSSBot() {
+        System.out.println("This robots name is " + name + ". This bots color is " + color + ". This bot helps you with " + functionality + ".");
+    }
+
+    public static void introduceSelfToJavaScriptBot() {
+        System.out.println("This robots name is " + name + ". This bots color is " + color + ". This bot helps you with " + functionality + ".");
+    }
+
+    public static void introduceSelfToCBot() {
+        System.out.println("This robots name is " + name + ". This bots color is " + color + ". This bot helps you with " + functionality + ".");
+    }
+
+    public static void introduceSelfToCPPBot() {
+        System.out.println("This robots name is " + name + ". This bots color is " + color + ". This bot helps you with " + functionality + ".");
+    }
+
+    public static void introduceSelfToCSharpBot() {
+        System.out.println("This robots name is " + name + ". This bots color is " + color + ". This bot helps you with " + functionality + ".");
+    }
+
+    //Make Robots!
+    public static void main(String[] args) {
+
+        //Robot 1
+        Robot PyPyHelp_Bot = new Robot();
+
+        PyPyHelp_Bot.name = "Py Py Help Bot";
+        PyPyHelp_Bot.color = "Blue & Yellow";
+        PyPyHelp_Bot.functionality = "Python";
+
+        introduceSelfToPythonBot();
+
+        //Robot 2
+        Robot JavaHelpBot = new Robot();
+
+        JavaHelpBot.name = "Java Help Bot";
+        JavaHelpBot.color = "Blue & Brown";
+        JavaHelpBot.functionality = "Java";
+
+        introduceSelfToJavaBot();
+
+        //Robot 3
+        Robot RubyHelpBot = new Robot();
+
+        RubyHelpBot.name = "Ruby Mine Bot";
+        RubyHelpBot.color = "Red";
+        RubyHelpBot.functionality = "Ruby";
+
+        introduceSelfToJavaBot();
+
+        //Robot 4
+        Robot HTMLHelpBot = new Robot();
+
+        HTMLHelpBot.name = "HTML Help Bot";
+        HTMLHelpBot.color = "Orange";
+        HTMLHelpBot.functionality = "HTML";
+
+        introduceSelfToHTMLBot();
+
+        //Robot 5
+        Robot CSSHelpBot = new Robot();
+
+        CSSHelpBot.name = "CSS Help Bot";
+        CSSHelpBot.color = "Blue";
+        CSSHelpBot.functionality = "CSS";
+
+        introduceSelfToCSSBot();
+
+        //Robot 6
+        Robot JavaScriptHelpBot = new Robot();
+
+        JavaScriptHelpBot.name = "JavaScript Help Bot";
+        JavaScriptHelpBot.color = "Yellow";
+        JavaScriptHelpBot.functionality = "JavaScript";
+
+        introduceSelfToJavaScriptBot();
+
+        //Robot 7
+        Robot CHelpBot = new Robot();
+
+        CHelpBot.name = "C Help Bot";
+        CHelpBot.color = "Dark Blue";
+        CHelpBot.functionality = "C";
+
+        introduceSelfToCBot();
+
+        //Robot 8
+        Robot CPPHelpBot = new Robot();
+
+        CPPHelpBot.name = "CPP/C++ Help Bot";
+        CPPHelpBot.color = "Light Blue";
+        CPPHelpBot.functionality = "CPP/C++";
+
+        introduceSelfToCPPBot();
+
+        //Robot 9
+        Robot CSharpHelpBot = new Robot();
+
+        CSharpHelpBot.name = "CSHARP/C# Help Bot";
+        CSharpHelpBot.color = "Dark Purple";
+        CSharpHelpBot.functionality = "CSHARP/C#";
+        
+        introduceSelfToCSharpBot();
+
+    }
+
+
+}
